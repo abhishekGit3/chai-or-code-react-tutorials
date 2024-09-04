e912f6a6dd9a48174a98b97aa7915ee45309c085
abhishekGit3 <154303090+abhishekGit3@users.noreply.github.com>
2024-09-04 09:55:00 +0000

    app write

--- appwriteMegaBlog/src/App.jsx
+++ appwriteMegaBlog/src/App.jsx
@@ -1,7 +1,11 @@
+
+import { useDispatch } from 'react-redux';
+import { useState , useEffect } from 'react'
+import authService from './appwrite/auth'
 import './App.css'
 
 function App() {
-  console.log("environment variables",import.meta.env.VITE_APPWRITE_URL)
+ const [loading , setloading] = useState(true);
   return (
     <>
       <h1>App write</h1>
