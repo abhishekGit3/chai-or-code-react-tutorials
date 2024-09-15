f23a0cc24bc07755ad115d108163dce0b282444e
abhishekGit3 <154303090+abhishekGit3@users.noreply.github.com>
2024-09-15 10:30:23 +0000

    appwrite

---
+++ appwriteMegaBlog/src/components/Select.jsx
@@ -0,0 +1,23 @@
+import React , {useId} from 'react'
+
+const Select = React.forwardRef( function Select({
+    option,
+    label,
+    className = '',
+    ...props
+},ref){
+    const id  = useId()
+    return (
+        <div className='w-full'>
+            {label && (
+                <label className='' htmlFor={id}></label>
+            )}
+         <select className={`px-3 py-2 rounded-lg bg-white text-black outline-none 
+            focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+            {...props} id={id} ref={ref}>
+            
+         </select>       
+         
+        </div>
+    )
+})
