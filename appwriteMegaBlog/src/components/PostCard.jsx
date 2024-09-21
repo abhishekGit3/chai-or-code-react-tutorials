9cb92af9d7c1a51f3e41414ed667fbb30c618fd2
abhishekGit3 <154303090+abhishekGit3@users.noreply.github.com>
2024-09-21 12:07:06 +0000

    appwrite

---
+++ appwriteMegaBlog/src/components/PostCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import appwriteService from '../appwrite/config'
+import {link} from 'react-router-dom'
+function PostCard({title , slug , content , featuredImage , status , userId}) {
+  return (
+    <div >
+        
+    </div>
+  )
+}
+
+export default PostCard
