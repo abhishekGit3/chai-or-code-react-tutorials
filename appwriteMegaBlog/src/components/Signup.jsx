691f34d3982c91a41df519b591c5307783f9fee8
abhishekGit3 <154303090+abhishekGit3@users.noreply.github.com>
2024-10-02 12:23:21 +0000

    appwrite

--- appwriteMegaBlog/src/components/Signup.jsx
+++ appwriteMegaBlog/src/components/Signup.jsx
@@ -3,8 +3,30 @@ import { useForm } from 'react-hook-form'
 import { useDispatch } from 'react-redux'
 import {Input , Button ,  Logo} from './index'
 import { useNavigate , Link } from 'react-router-dom'
+import authSlice, { login } from '../store/authSlice'
+import authService from '../appwrite/auth'
+import { register , handleSubmit} from 'react-hook-form'
 
 function Signup() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [error , setError] = useState();
+  const {register , handleSubmit} = useForm();
+
+ const create = async(data)=>{
+    setError("");
+    try {
+      const userData = await authService.createAccount(data);
+      if(userData){
+        const userData = authService.getCurrentUser();
+        dispatch(authSlice.login(userData));
+      }
+
+    } catch (error) {
+      setError(error.message);
+    }
+ }
   return (
     <div>
       
