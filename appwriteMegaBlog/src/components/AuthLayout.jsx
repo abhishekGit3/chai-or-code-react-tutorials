10a11d4b2ea766e9dffe8b88ca177c8846a6a6c9
abhishekGit3 <154303090+abhishekGit3@users.noreply.github.com>
2024-10-03 09:20:29 +0000

    appwrite

--- appwriteMegaBlog/src/components/AuthLayout.jsx
+++ appwriteMegaBlog/src/components/AuthLayout.jsx
@@ -3,21 +3,25 @@ import { useSelector } from 'react-redux'
 import { useState , useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-function Protected({children , authentication}) {
+function Protected({children , authentication = true}) {
     const navigate = useNavigate();
-    const [loader , setLoader] = useState("");
+    const [loader , setLoader] = useState(true);
 
     const authStatus = useSelector((state) => state.auth.status)
 
     useEffect(()=>{
-
-    },[ authStatus , authentication , navigate ])
-
-  return (
-    <div>
       
-    </div>
-  )
+      if(authentication && authStatus !== authentication)
+      {
+        navigate("/login")
+
+      }else if(!authentication && authStatus !== authentication){
+        navigate("/")
+      }
+      setLoader(false)
+    },[ authStatus , authentication , navigate ])
+     
+    return loader ? <h1>...Loading</h1> : <>{children}</>
 }
 
 export default Protected
