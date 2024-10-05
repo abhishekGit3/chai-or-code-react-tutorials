f0524bb8bdd8e18fe15b1c612f5b6360fcffcb81
abhishekGit3 <154303090+abhishekGit3@users.noreply.github.com>
2024-10-05 15:25:48 +0000

    appwrite

--- appwriteMegaBlog/src/components/post-form/PostForm.jsx
+++ appwriteMegaBlog/src/components/post-form/PostForm.jsx
@@ -1,1 +1,32 @@
 import RTE from '../RTE'
+import {useSelector} from 'react-redux'
+import { useForm } from 'react-hook-form'
+import {useNavigate} from 'react-router-dom'
+import { useCallback , useState } from 'react'
+import authService from '../../appwrite/auth'
+import {Button , Input , Select , RTE} from '../index'
+
+import React from 'react'
+
+function PostForm({post}) {
+    const {register , handleSubmit , watch ,  setValue ,
+    control , getValues} = useForm({
+        defaultValues: {
+          title : post?.title || '',
+          slug : post?.slug || '',
+          content : post?.content || '',
+          status : post?.status || ''
+        },
+    });
+
+
+    
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostForm
