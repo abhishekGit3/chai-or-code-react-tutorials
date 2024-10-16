977c4e3597402ac2983763ce7b5978bc137753f5
abhishekGit3 <154303090+abhishekGit3@users.noreply.github.com>
2024-10-16 15:27:38 +0000

    appwrite

--- appwriteMegaBlog/src/components/index.js
+++ appwriteMegaBlog/src/components/index.js
@@ -1,10 +1,13 @@
-
+import RTE from './RTE';
 import Logo from './Logo'
 import Input from './Input';
+import Login from './Login'
 import Button from './Button';
+import Signup from './Signup';
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import LogoutBtn from "./Header/LogoutBtn";
 import Container from "./container/Container";
-import RTE from './RTE';
-export { Header ,  Footer , Logo , Container , LogoutBtn , Button , Input , RTE}
+import PostForm from './post-form/PostForm';
+export { Header ,  Footer , Logo , Container ,
+LogoutBtn , Button , Input , RTE , Signup , Login , PostForm}
